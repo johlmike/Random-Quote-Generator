75e4b38c9d845267da06a3644dffd162a74c0fa8
johlmike <johlmike@gmail.com>
2016-04-28 19:58:03 +0800

    Complete most function

---
+++ js/quote_list.js
@@ -0,0 +1,84 @@
+//Find quotes from :  http://www.brainyquote.com/
+
+var quoteList = [
+    {
+        quote: "Good, better, best. Never let it rest. 'Til your good is better and your better is best.",
+        source: 'St. Jerome',
+        genre: 'motivational',
+        citation: '',
+        year: '',
+        displayed: false
+    },
+    {
+        quote: "In order to succeed, we must first believe that we can.",
+        source: 'Nikos Kazantzakis',
+        genre: 'motivational',
+        citation: '',
+        year: '',
+        displayed: false
+    },
+    {
+        quote: "Always remember that you are absolutely unique. Just like everyone else.",
+        source: 'Margaret Mead',
+        genre: 'funny',
+        citation: '',
+        year: '',
+        displayed: false
+    },
+    {
+        quote: "People who think they know everything are a great annoyance to those of us who do.",
+        source: 'Isaac Asimov',
+        genre: 'funny',
+        citation: '',
+        year: '',
+        displayed: false
+    },
+    {
+        quote: "Friendship is always a sweet responsibility, never an opportunity.",
+        source: 'Khalil Gibran',
+        genre: 'friendship',
+        citation: '',
+        year: '',
+        displayed: false
+    },
+    {
+        quote: "The greatest gift of life is friendship, and I have received it.",
+        source: 'Hubert H. Humphrey',
+        genre: 'friendship',
+        citation: '',
+        year: '',
+        displayed: false
+    },
+    {
+        quote: "In every conceivable manner, the family is link to our past, bridge to our future.",
+        source: 'Alex Haley',
+        genre: 'family',
+        citation: '',
+        year: '',
+        displayed: false
+    },
+    {
+        quote: "Family is not an important thing. It's everything.",
+        source: 'Michael J. Fox',
+        genre: 'family',
+        citation: '',
+        year: '',
+        displayed: false
+    },
+    {
+        quote: "Love isn't something you find. Love is something that finds you.",
+        source: 'Loretta Young',
+        genre: 'love',
+        citation: '',
+        year: '',
+        displayed: false
+    },
+    {
+        quote: "True love is like ghosts, which everyone talks about and few have seen.",
+        source: 'Francois de La Rochefoucauld',
+        genre: 'love',
+        citation: '',
+        year: '',
+        displayed: false
+    }
+];
--- js/script.js
+++ js/script.js
@@ -2,3 +2,66 @@
 // when user clicks anywhere on the page, the "makeQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+function randomRGB() {
+    var rgbString = 'rgb(';
+    rgbString += Math.floor( Math.random() * 257 ) + ',';
+    rgbString += Math.floor( Math.random() * 257 ) + ',';
+    rgbString += Math.floor( Math.random() * 257 ) + ')';
+    return rgbString;
+}
+
+function getRandomQuote() {
+    if( quoteListTemp.length === 0 ){
+        quoteListTemp = quoteList;
+        for( var i = 0; i < quoteListTemp.length ; i += 1 ){
+            quoteListTemp[i].displayed = false;
+        }
+    }
+    var randomIndex = Math.floor( Math.random() * quoteListTemp.length );
+    quoteListTemp[randomIndex].displayed = true;
+    return quoteListTemp[randomIndex];
+}
+
+
+function printQuote() {
+    //Random a quote from temp quote list
+    var quoteObj = getRandomQuote(); //get an quote object
+    var nDisplayedList = [];
+    var html = '<p class="quote">';
+
+    //Get element by ID and body tag
+    var divQuote = document.getElementById('quote-box');
+    var bodyTag = document.getElementsByTagName('body');
+
+    //Put quote into html
+    html += quoteObj['quote'] + '</p> <p class="source">';
+    html += quoteObj['source'];
+
+    if( quoteObj['citation'] !== ''){
+        html += '<span class="citation">' + quoteObj['citation'] + '</span>';
+    }
+
+    if( quoteObj['year'] !== null){
+        html += '<span class="citation">' + quoteObj['year'] + '</span>';
+    }
+
+    html += '</p>';
+
+    divQuote.innerHTML = html;
+
+    //Random background color
+    bodyTag[0].style.backgroundColor = randomRGB();
+
+    //Check quotes that  showed before
+    console.log( quoteObj['quote'] );
+
+    //Sort not displayed  quotes and put it back to the quoteListTemp
+    for( var i = 0 ; i < quoteListTemp.length ; i += 1){
+        if( quoteListTemp[i].displayed === false ){
+            nDisplayedList.push( quoteListTemp[i] );
+        }
+    }
+    quoteListTemp = nDisplayedList;
+}
+
+var quoteListTemp = quoteList;
